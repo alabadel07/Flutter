5afe08e4c82e230d6ae5094e4515ba0d1c737bda
Alabadel07 <abdel.rachouani103@outlook.fr>
2024-05-24 13:58:47 +0200

    get products linked to the category and set up the display process

--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -1,42 +1,44 @@
 import 'dart:convert';
-import 'dart:developer';
-
 import 'package:http/http.dart' as http;
 import 'package:store/config/config.dart';
 import 'package:store/models/product.dart';
 
 class ProductsService {
-  /*
-    récupérer tous les produits
-    Future équivalent d'une Promise en JS
-  */
   Future<List<Product>> getProducts() async {
-    // réquête en GET
+    // Effectuer une requête GET pour récupérer tous les produits
     Uri uri = Uri.parse('${Config.API_URL}/products');
     http.Response response = await http.get(uri);
 
-    // récupérer la réponse
+    // Vérifier le statut de la réponse
     if (response.statusCode == 200) {
-      // convertir les données en JSON
-      List data = jsonDecode(response.body);
+      // Convertir les données JSON en une liste de produits
+      List<dynamic> data = jsonDecode(response.body);
+      List<Product> products = data.map((value) {
+        return Product(
+          id: value['id'],
+          title: value['title'],
+          price: value['price'],
+          description: value['description'],
+          category: value['category'],
+          image: value['image'],
+          rating: value['rating'],
+        );
+      }).toList();
+      return products;
+    } else {
+      // En cas d'erreur, lancer une exception
+      throw Exception('Failed to load products');
+    }
+  }
 
-      // renvoyer une liste d'objet
-      return data
-          .map(
-            (dynamic value) => Product(
-                id: value['id'],
-                title: value['title'],
-                price: value['price'],
-                description: value['description'],
-                category: value['category'],
-                image: value['image'],
-                rating: value['rating']),
-          )
-          .toList();
-    }
-    // renvoyer une erreur
-    else {
-      throw Error();
-    }
+  Future<List<Product>> getProductsByCategory(String category) async {
+    // Récupérer tous les produits
+    List<Product> allProducts = await getProducts();
+
+    // Filtrer les produits par catégorie
+    List<Product> categoryProducts =
+        allProducts.where((product) => product.category == category).toList();
+
+    return categoryProducts;
   }
 }
