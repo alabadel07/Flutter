dc540d4401acf749f403dc5aeb3117d282c72576
Abdellah Rachouani <abdel.rachouani103@outlook.fr>
2024-06-02 22:21:05 +0200

    adding CartProviderin the main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+// lib/main.dart
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:store/providers/category_provider.dart';
 import 'package:store/providers/product_provider.dart';
+import 'package:store/providers/cart_provider.dart';
 import 'package:store/screens/category_products_screen.dart';
+import 'package:store/screens/cart_screen.dart';
 import 'package:store/services/router_service.dart';
 import 'package:store/widgets/common/drawer_widget.dart';
 
@@ -14,6 +18,9 @@ void main() => runApp(
           ),
           ChangeNotifierProvider(
             create: (context) => CategoryProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => CartProvider(),
           ),
         ],
         child: const Store(),
@@ -38,6 +45,18 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => CartScreen(),
+                ),
+              );
+            },
+          ),
+        ],
       ),
       drawer: DrawerWidget(),
       body: Center(
